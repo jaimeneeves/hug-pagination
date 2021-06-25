92b4399f38efd26a2ba3025c09cc07c9857c88ef
Jaime Neves <jaime.neves@reclameaqui.com.br>
2021-06-25 11:46:03 -0300

    Atualizaçao do arquivo webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-function component() {
-  const element = document.createElement('div');
+import capitalize from './capitalize';
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = `Hug Pagination - 2`;
-
-  return element;
-}
-
-document.body.appendChild(component());
+export { capitalize };
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,40 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    index: './src/index.js',
-  },
+  entry: path.resolve(__dirname, 'src/index.js'),
   devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   devServer: {
     contentBase: './dist',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-  ],
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     title: 'Development',
+  //   }),
+  // ],
   output: {
-    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    filename: 'HugPagination.js',
+    library: {
+      name: 'HugPagination',
+      type: 'umd'
+    },
+    // clean: true,
     publicPath: '/',
   },
 };
