1b234785360e966735ac86cc128a58897088d851
Jaime Neves <jaime.neves@reclameaqui.com.br>
2021-07-24 11:17:13 -0300

    update webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'HugPagination.js',
+    filename: 'hugpagination.js',
     library: {
       name: 'HugPaginationJS',
       type: 'umd'
